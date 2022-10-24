105e632cb58fe4571cd7a0aaa00d753051c912d7
dhermys <96082998+dhermys@users.noreply.github.com>
2022-10-24 20:33:27 -0300

    Commit inicial curso Persistência com Flutter aula 3

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:bytebank2/models/contact.dart';
+import 'package:bytebank2/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+import 'database/app_database.dart';
+
+void main() {
+  runApp(BytebankApp());
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+}
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.green[900], secondary: Colors.blueAccent[700]),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+
