cd65da43c6afc36f53894eb947052ced133ac27d
dhermys <96082998+dhermys@users.noreply.github.com>
2022-10-26 16:26:20 -0300

    Flutter com Web API Aula 2.6

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,7 +1,30 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Request');
+    print('url: ${data.baseUrl}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print('url: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
   var baseUrl = Uri.parse('http://192.168.0.173:8080/transactions');
-  final Response response = await get(baseUrl);
-  print(response.body);
+  final Response response = await client.get(baseUrl);
 }
